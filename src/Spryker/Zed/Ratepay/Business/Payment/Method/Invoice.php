98a1047fa898b9e2504fb3d49a5831a8ac420276
Volodymyr Lunov <volodymyr.lunov@spryker.com>
2016-04-07 10:26:23 +0200

    Ratepay: Fixed transaction id setting.

--- src/Spryker/Zed/Ratepay/Business/Payment/Method/Invoice.php
+++ src/Spryker/Zed/Ratepay/Business/Payment/Method/Invoice.php
@@ -44,8 +44,7 @@ class Invoice extends AbstractMethod
          * @var \Spryker\Zed\Ratepay\Business\Api\Model\Payment\Request $request
          */
         $request = $this->modelFactory->build(ApiConstants::REQUEST_MODEL_PAYMENT_REQUEST);
-        $request->getHead()->setTransactionId($paymentData->getTransactionId())
-            ->setTransactionShortId($paymentData->getTransactionShortId());
+        $request->getHead()->setTransactionId($paymentData->getTransactionId());
 
         $this->converter->mapCustomer($quoteTransfer, $paymentData, $request->getCustomer());
         $this->converter->mapBasket($quoteTransfer, $request->getShoppingBasket());
